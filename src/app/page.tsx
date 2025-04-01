1123bb3273f21f2762c145db56552857fbf97e1e
pedrofelip <pedrofelipbarros@gmail.com>
2025-04-01 13:04:04 -0300

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,11 +21,23 @@ export default function Home() {
                 <a href="#" className={styles.about_btn}>Sobre mim</a>
               </div>
             </div>
-            <div>
+            <div className={styles.img_content}>
               <img src="./developer-activity-animate.svg" alt="" />
             </div>
           </div>
           <ScrollDownButton />
+        </section>
+        <section className={styles.second_section}>
+          <div className={`${styles.grid_container} ${styles.content_second_section}`}>
+            <div>
+
+            </div>
+            <div className={styles.text_content_second_section}>
+              <h2>Sobre mim</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum laudantium quae pariatur similique repudiandae voluptas sint dolorum ullam nostrum tenetur fuga numquam eveniet, consequuntur rem cupiditate vero explicabo dicta a neque rerum natus non necessitatibus ducimus temporibus! Rerum odit repellat reiciendis ipsa iste amet ipsum deserunt maxime sed, adipisci aperiam.</p>
+              <a href="#" className={styles.send_message_btn}>Baixar currículo</a>
+            </div>
+          </div>
         </section>
       </main>
     </>
